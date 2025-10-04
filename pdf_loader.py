967410190e6a93a4feb1cf22139153f1e2e55958
Lavudya Swamy <lavudyaswamy4@gmail.com>
2025-10-04 10:03:11 +0530

    Update pdf_loader.py

--- pdf_loader.py
+++ pdf_loader.py
@@ -1,5 +1,6 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader      # basically we are working on the document loader some of the steps are constant like this first-step and which loadr
 
+                                                                     we are used ove there just mention the loader 
 
 loader = PyPDFLoader('dl-curriculum.pdf')  
 
@@ -9,4 +10,5 @@ docs = loader.load()
 print(docs)
 print(len(docs))
 print(docs[0].page_content)
+
 print(docs[1].metadata)
